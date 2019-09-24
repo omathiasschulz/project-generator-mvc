ad985b601924e58fe1e03df15a01650f16a32372
Mathias Artur Schulz <MathiasSchulz34@gmail.com>
2019-09-23 21:58:53 -0300

    [UPD] Atualizando a criação dos folders

---
+++ generator/GenerateFolders.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once('Helpers.php');
+
+class GenerateFolders
+{
+    /**
+     * Método responsável por gerar os folders
+     */
+    public static function create($aFolders)
+    {
+        foreach ($aFolders as $folder) {
+            Helpers::createFolder('../' . $folder);
+        }
+    }
+}
